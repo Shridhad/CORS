362742942cf947b06faa05fe495f3593f09f8703
Shridhar Deshmukh <shridhar.deshmukh3@gmail.com>
2014-02-23 13:12:16 +0530

    No Transport Error

--- origin/app/assets/javascripts/welcome.js
+++ origin/app/assets/javascripts/welcome.js
@@ -1,14 +1,22 @@
 $(document).ready(function(){
-    $.ajax({
-        type: 'GET',
-        url: 'http://locale.resource.com:3001/text',
-        dataType: 'text',
-        success: function(data, status, xhr) {
-            console.log("Success ", data);
-        },
-        error: function(xhr, status, error){
-            console.log("error ", xhr);
-        }
+
+    $("#btn").on('click', function(){
+        var error = function(xhr, status, error){
+                console.log("Error " + error);
+                $(".container .response").html("<p>" + status + ": " + error + "</p>");
+            },
+            success= function(data, status, xhr){
+                console.log("success ", data);
+                $(".container .response").html("<p>" + status + ": " + data + "</p>");
+            };
+        $.ajax({
+            type: 'GET',
+            cache:false,
+            url: 'http://locale.resource.com:3001/text',
+            dataType: 'text',
+            success: success,
+            error: error
+        })
     })
 
 });
